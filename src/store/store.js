39df23c74d3186892b6ac156989ea32f5a979812
0xhaz <28639279+0xhaz@users.noreply.github.com>
2022-09-13 21:15:40 +0800

    change middleware to its own directory

--- src/store/store.js
+++ src/store/store.js
@@ -2,22 +2,9 @@ import { compose, createStore, applyMiddleware } from "redux";
 // import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import { loggerMiddleware } from "./middleware/logger";
 
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddleware = store => next => action => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("type", action.type);
-  console.log("payload", action.payload);
-  console.log("currentState", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
 
 const persistConfig = {
   key: "root",
